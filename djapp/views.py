43a5ecaa134ef5c9f1aa05594304738ca38297b8
MarwaEzzatMo <marwaezzat201@gmail.com>
2022-04-23 10:45:34 +0200

    day4

--- djapp/views.py
+++ djapp/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from .models import Student, Track
 
 # Create your views here.
+
+def home(request):
+    all_students = Student.objects.all()
+    return render(request, 'djapp/home.html', {'student_list':all_students})
+
+def show(request , st_id):
+    st = Student.objects.get(id=st_id)
+    context = {'st': st }
+    return render(request,'djapp/show.html',context)
+
+
+def del_st(request , st_id):
+    st = Student.objects.get(id=st_id)
+    st.delete()
+    return redirect('home')
+       
+       
+
